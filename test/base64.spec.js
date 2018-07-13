491e2a2bb0e210d713e89f9ed5551cea002ffafd
edoner <edonet@163.com>
2018-07-13 18:11:47 +0800

    add: base64

---
+++ test/base64.spec.js
@@ -0,0 +1,32 @@
+/**
+ *****************************************
+ * Created by lifx
+ * Created on 2018-07-13 16:38:51
+ *****************************************
+ */
+'use strict';
+
+
+/**
+ *****************************************
+ * 加载模块
+ *****************************************
+ */
+import { decode, encode } from '../lib/base64';
+
+
+/**
+ *****************************************
+ * 测试模块
+ *****************************************
+ */
+describe('测试【base64】', () => {
+    test('【base64】编码解码', () => {
+        expect(decode(encode('中国'))).toBe('中国');
+        expect(encode(decode('5Lit5Zu9'))).toBe('5Lit5Zu9');
+        expect(encode('中国人')).toBe('5Lit5Zu95Lq6');
+        expect(decode('5Lit5Zu95Lq6')).toBe('中国人');
+        expect(encode('中国人')).toBe(new Buffer('中国人').toString('base64'));
+        expect(decode('5Lit5Zu95Lq6')).toBe(new Buffer('5Lit5Zu95Lq6', 'base64').toString());
+    });
+});
